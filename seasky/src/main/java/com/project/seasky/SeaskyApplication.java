5a71895cbb6e295e80d578da13ea492106aeb259
Jayant Anand <129861458+jay-and-main@users.noreply.github.com>
2025-03-10 18:40:15 +0530

    Initial commit

---
+++ seasky/src/main/java/com/project/seasky/SeaskyApplication.java
@@ -0,0 +1,13 @@
+package com.project.seasky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaskyApplication.class, args);
+	}
+
+}
---
+++ seasky/src/test/java/com/project/seasky/SeaskyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.seasky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
