9d8d88b6795205e91da3c19b266fc636058ade5f
Jayant Anand <129861458+jay-and-main@users.noreply.github.com>
2025-03-12 13:37:29 +0530

    Made small html mappings

---
+++ seasky/src/main/java/com/project/seasky/controller/ViewController.java
@@ -0,0 +1,26 @@
+package com.project.seasky.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ViewController {
+    @GetMapping("/")
+    public String showLogin() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegister() {
+        return "register";
+    }
+
+    @GetMapping("/home")
+    public String showHome() {
+        return "index";
+    }
+}
