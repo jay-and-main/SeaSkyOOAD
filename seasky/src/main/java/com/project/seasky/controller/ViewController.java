b88afe3f4f5450a3c3be154d28c076e09124e780
Jayant Anand <129861458+jay-and-main@users.noreply.github.com>
2025-03-20 05:58:59 +0530

    Added logic  for booking and shipping

--- seasky/src/main/java/com/project/seasky/controller/ViewController.java
+++ seasky/src/main/java/com/project/seasky/controller/ViewController.java
@@ -26,6 +26,51 @@ public class ViewController {
         return "bookings";
     }
 
+    @GetMapping("/flight")
+    public String showFlight() {
+        return "flight";
+    }
+
+    @GetMapping("flight-suggestions")
+    public String showFlightSuggestions() {
+        return "flight-suggestions";
+    }
+
+    @GetMapping("/parcel")
+    public String showParcel(){
+        return "parcel";
+    }
+
+    @GetMapping("/parcel-suggestions")
+    public String showParcelSuggestions(){
+        return "parcel-suggestions";
+    }
+
+    @GetMapping("/past-bookings")
+    public String showPastBookings(){
+        return "past-bookings";
+    }
+
+    @GetMapping("/admin")
+    public String showAdmin(){
+        return "admin-home";
+    }
+
+    @GetMapping("/admin-no-fly")
+    public String showAdminNoFly(){
+        return "admin-no-fly";
+    }
+
+    @GetMapping("/admin-booking-status")
+    public String showAdminBookingStatus(){
+        return "admin-booking-status";
+    }
+
+    @GetMapping("/admin-shipment-status")
+    public String showAdminShipmentStatus(){
+        return "admin-shipment-status";
+    }
+
     @GetMapping("/error")
     public String showError() {
         return "error";
